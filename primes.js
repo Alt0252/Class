7f4f4c2b99e03fb84820a3e6d47ffa3ab8d82c98
Alt0252 <will0527@gmail.com>
2013-06-24 20:16:05 -0700

    Assignment 1 part 2

---
+++ primes.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var outfile = "primes.txt";
+
+var num_primes = 0
+for (var i = 1; num_primes <=5; i++) {
+    if (checkPrime (i)) {
+	fs.writeFileSync(outfile, i + ",");
+	console.log ("Wrote: " + i + " as prime.\n");
+    }
+}
+ 
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
+
+function checkPrime (num) {
+    if (num < 2) return false; 
+
+    var last = (int) Math.sqrt (num); //the square root of num determines the test set  
+
+    //check if evenly divisible up to the square root.
+    for (var i = 2; i <= last; i++) {
+        if (num % i == 0) {
+            return false; //factor found
+        }
+    }
+
+    return true; // no factor found.  Number is prime
+}
